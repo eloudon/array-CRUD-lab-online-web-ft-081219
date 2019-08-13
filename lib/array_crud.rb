e491b4869c3dc07758c02afa23671fa5eb8e0795
Eric Loudon <loudon.eric@gmail.com>
2019-08-13 08:57:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-array.pop("arrays!")
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array.shift("wow")
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
